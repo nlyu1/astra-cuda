860fe4862426108dce738154e9137e78c08d8258
nlyu <lyuxingjian@gmail.com>
2025-07-29 22:54:24 -0400

    initial updates

--- build/build-release/CMakeFiles/fc-tmp/fmt/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for fmt")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/nlyu/Code/astra-cuda/build/build-release/CMakeFiles/fc-tmp/fmt/fmt-gitupdate.cmake")
-
-endblock()
--- build/build-release/CMakeFiles/fc-tmp/pybind11/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for pybind11")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/home/nlyu/Code/astra-cuda/build/build-release/CMakeFiles/fc-tmp/pybind11/pybind11-gitclone.cmake")
-
-endblock()
