d688d0f335d3c34bdcc691e349170647bc023f81
nlyu <lyuxingjian@gmail.com>
2025-07-30 07:42:10 -0400

    Fix .gitignore and remove build directory from tracking

--- build/build-release/CMakeFiles/fc-tmp/pybind11/patch.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing patch step for pybind11")
-
-block(SCOPE_FOR VARIABLES)
-
-
-
-endblock()
--- build/build-release/pybind/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nlyu/Code/astra-cuda/src/pybind
-# Build directory: /home/nlyu/Code/astra-cuda/build/build-release/pybind
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
